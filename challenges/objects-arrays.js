e6ce6303112206447489aac05103172b36f0f934
maribelcuales <maribelcuales@gmail.com>
2020-04-17 01:50:38 +0800

    Completed lowCaseAnimalNames array returning animal names in lowercase.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -138,7 +138,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((animal) => {
+  return `${animal.animal_name.toLowerCase()}`;
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
