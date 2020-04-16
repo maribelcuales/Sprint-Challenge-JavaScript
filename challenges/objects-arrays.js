17f785da563220f64d39c9ba2da5112c7010de2a
maribelcuales <maribelcuales@gmail.com>
2020-04-16 20:54:31 +0800

    Completed const unisWithUni returning universities with the string 'Uni' in its name.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -93,7 +93,14 @@ const contactInfo = graduates.map(function(graduate){
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter(function(univ){
+  if(univ.university.includes('Uni')){
+    return true;
+  } else {
+    return false;
+  }
+  return univ.university;
+})
 console.log(unisWithUni);
 
 
