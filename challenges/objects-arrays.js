6bb0fa1296711c16420107ba284e84e689fab74a
maribelcuales <maribelcuales@gmail.com>
2020-04-17 02:38:08 +0800

    Refactored lowCaseAnimalNames to an arrow function.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -138,9 +138,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map((animal) => {
-  return `${animal.animal_name.toLowerCase()}`;
-})
+const lowCaseAnimalNames = zooAnimals.map((animal) => `${animal.animal_name.toLowerCase()}`)
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
